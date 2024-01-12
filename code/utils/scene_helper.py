f078d7451ebd09d0c67b437ab04cdcd9ab9510a9
Jakob <jakobdanel@aol.com>
2024-01-12 09:06:58 +0100

    Fixes: Checking correctly for labeled videos and fixed regex

--- code/utils/scene_helper.py
+++ code/utils/scene_helper.py
@@ -2,17 +2,18 @@ import re
 import os
 from utils.helper import read_ndjson
 from utils.paths import get_annotations_path
-from utils.labelbox_to_coco import get_video_location
+from utils.labelbox_to_coco import get_video_location, video_is_labeled
 from utils.paths import get_video_dir
+
 def remove_pattern_from_string(input_string):
-    pattern = r"_\d+_\d+\.mp4"
+    pattern = r"_\d{3}-\d{3}_\d+\.mp4|_\d+_\d+\.mp4"
     result_string = re.sub(pattern, "", input_string)
     return result_string
 
 
 def get_all_scenes():
     data = read_ndjson(get_annotations_path())
-    data = [d for d in data if os.path.exists(get_video_location(get_video_dir("original"),d))]
+    data = [d for d in data if video_is_labeled(d)]
     scenes = []
     for entry in data:
         scene_name = remove_pattern_from_string(entry["data_row"]["external_id"])
@@ -22,6 +23,7 @@ def get_all_scenes():
 
 def get_scene_to_video_combination():
     data = read_ndjson(get_annotations_path())
+    data = [d for d in data if video_is_labeled(d)]
     comb = dict()
     for i in range(len(data)):
         str = remove_pattern_from_string(data[i]["data_row"]["external_id"])
