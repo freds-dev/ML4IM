e09f96e67dbdccd82f7886ad7c855d56300a2a96
Jakob <jakobdanel@aol.com>
2024-02-12 14:16:19 +0100

    Transforming both frames according to mapping

--- code/utils/file_system.py
+++ code/utils/file_system.py
@@ -1,7 +1,16 @@
 import os
 import cv2
+import numpy as np
 
+from utils.video_calibration import *
 from utils.helper import adjust_string_length
+
+# Kein Aenderung
+def show_image(im):
+    cv2.imshow('image',im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 def create_directory(directory_path):
     # Check if the directory already exists
@@ -12,7 +21,7 @@ def create_directory(directory_path):
     else:
         print(f"Directory '{directory_path}' already exists")    
 
-def save_frames_from_video(video_path_event,video_path_rgb,rg_index,output_folder, number_frames, video_id):
+def save_frames_from_video(video_path_event,video_path_rgb,config,output_folder, number_frames, video_id):
     # Open the video file
     cap_event = cv2.VideoCapture(video_path_event)
     cap_rgb = cv2.VideoCapture(video_path_rgb)
@@ -33,10 +42,20 @@ def save_frames_from_video(video_path_event,video_path_rgb,rg_index,output_folde
 
             # Save the frame as an image
             frame_filename = f'img_{video_id}_{adjust_string_length( str(frame_number + 1),6,"0")}.png'
+            
             frame_event = crop_image(frame_event)
-            frame_rgb = crop_image(frame_rgb)
-            frame = cv2.cvtColor(frame_event, cv2.COLOR_RGB2RGBA)
-            frame[:,:,3] = 255 #TODO: Use really the loaded data instead of constant
+            frame_rgb = crop_image_rgb(frame_rgb)
+            
+            frame_event = cv2.resize(frame_event, dsize=(1280, 720))
+
+            frame_event,_ = undistort_img(frame_event)
+            frame_rgb,_ = undistort_img(frame_rgb, False)
+            cv2.imwrite("tmp_event.png",frame_event)
+            cv2.imwrite("tmp_rgb.png",frame_rgb)
+            frame_rgb = translate_img(frame_rgb,frame_event.shape[1],frame_event.shape[0])
+            cv2.imwrite("tmp_mapped.png",frame_rgb)
+            
+            frame = config.build_frame(frame_event,frame_rgb) 
             cv2.imwrite(os.path.join(str(output_folder), frame_filename),frame)
             #bar()
             if frame_count % 100 == 0:
@@ -56,7 +75,23 @@ def write_file(path,content):
     f.close()
     print(f"Saved content to {path}")
     
-    
+
+def get_homography_matrix(x=0,y=12):   
+    calibration_file = get_homography()
+    fs = cv2.FileStorage(calibration_file, cv2.FILE_STORAGE_READ)
+    homography_matrix = fs.getNode('H').mat()
+    homography_matrix = homography_matrix @ np.array([ [1, 0, x], [0, 1, y], [0, 0, 1] ], dtype=np.float32)
+    fs.release()
+    return homography_matrix
 
 def crop_image(frame):
     return frame[:1080, :]
+
+def crop_image_rgb(image_matrix, target_height = 1200):
+    height, width, channels = image_matrix.shape
+    start_row = max(height - target_height, 0)
+
+    cropped_image_matrix = image_matrix[start_row:, :, :]
+
+    return cropped_image_matrix
+
