9e7411f52a1600d49f20155524effa6fac4ebb42
Jakob <jakobdanel@aol.com>
2024-02-20 16:48:52 +0100

    Add an dependency so that preprocessings are only runned once at a time

--- code/start_scene_cross_validation.py
+++ code/start_scene_cross_validation.py
@@ -3,12 +3,21 @@ import argparse
 
 from utils.scene_helper import get_all_scenes
 
-def start_scene_cross_validation(dataset_name,video_event_name,video_rgb_name,config_name):
+def start_scene_cross_validation(dataset_name,video_event_name,video_rgb_name,config_name, exception_scenes = []):
     scenes = get_all_scenes()
     
+    for scene in exception_scenes:
+        try:
+            scenes.remove(scene)
+        except ValueError:
+            print(f"Scene {scene} is not in the list of scenes")
+        
+    id = None
     for scene in scenes:
         # Run the Bash script with arguments
-        subprocess.run(['bash', "create_scene_split.sh", dataset_name, scene,video_event_name,video_rgb_name,config_name])
+        if id is None:
+            id = -1
+        id = subprocess.check_output(['bash', "create_scene_split.sh", dataset_name, scene,video_event_name,video_rgb_name,config_name,str(id)]).strip().split("\n")[-1]
         
         
 if __name__ == "__main__":
@@ -17,7 +26,8 @@ if __name__ == "__main__":
     parser.add_argument("-video_event_name", type=str, required=True,help="Directory where the event videos are located")
     parser.add_argument("-video_rgb_name", type=str, required=True,help="Directory where the rgb videos are located")
     parser.add_argument("-config_name",type=str,required=True,help="Name of the configuration file")
+    parser.add_argument('-exception_scenes', nargs='+', default=[], help='Array of scnees that will not be validated')
    
 
     args = parser.parse_args()
-    start_scene_cross_validation(args.dataset,args.video_event_name,args.video_rgb_name,args.config_name)
+    start_scene_cross_validation(args.dataset,args.video_event_name,args.video_rgb_name,args.config_nameargs.exception_scenes)
