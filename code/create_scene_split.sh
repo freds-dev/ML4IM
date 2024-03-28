59f53daeb3032805e8c52afd42e3ef191e782414
Jakob <jakobdanel@aol.com>
2024-03-28 14:20:29 +0100

    Revert "Add option to run a dry run"
    This reverts commit 249af158ab8a716dce5d05c98e33e0c52bd5df5c.

--- code/create_scene_split.sh
+++ code/create_scene_split.sh
@@ -4,20 +4,14 @@ echo "Build a datset for project $1 and scene $2"
     python utils/build_split_script.py --project_name $1 --scene_name $2 --video_event_name $3 --video_rgb_name $4 --config_name $5
 if [ "$6" -eq -1 ]; then
     echo "No dependency"
-    if ["$7" -eq 1]; then 
-        id=$(sbatch sbatch/pp/$1/$2.sh| { read text; echo ${text##* };})
-    fi
+    id=$(sbatch sbatch/pp/$1/$2.sh| { read text; echo ${text##* };})
 else
     echo "Dependency: $6"
-    if ["$7" -eq 1]; then
-        id=$(sbatch --dependency=afterok:$6 sbatch/pp/$1/$2.sh| { read text; echo ${text##* };})
-    fi
+    id=$(sbatch --dependency=afterok:$6 sbatch/pp/$1/$2.sh| { read text; echo ${text##* };})
 fi
 echo "Submitted preprocessing for $i split on id: $id"
 python utils/build_sbatch.py --script_location sbatch/train/$1/$2.sh --gpu_dataset $1.$2 --index $2 --project_name $1
-if ["$7" -eq 1]; then
-    sbatch --dependency=afterok:$id sbatch/train/$1/$2.sh
-fi
+sbatch --dependency=afterok:$id sbatch/train/$1/$2.sh
 echo "\n\n\n"
 echo $id
 
